dfaabf9329621df9f4ff3c709451678d98847c8c
gogogohuang <gogogohuang@gmail.com>
2020-08-28 21:42:44 +0800

    first commit

---
+++ config/V.ts
@@ -0,0 +1,3 @@
+
+export const apiKey = "CWB-27A80F1A-A586-4FDC-BE8A-641BF50848FA";
+export const seaData = "O-A0019-001";
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var HomePage = function () {
+    return react_1["default"].createElement("div", null, "Welcome to Next.js!");
+};
+exports["default"] = HomePage;
